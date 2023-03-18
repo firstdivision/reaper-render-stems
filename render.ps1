9c152ff84e43f72591dd83a630d18f6bf4546158
realkillfly@gmail.com <realkillfly@gmail.com>
2023-03-18 12:22:46 -0500

    Copy to new project file

--- render.ps1
+++ render.ps1
@@ -1,14 +1,28 @@
-$input_file="./Reaper_Projects/CleanProjectInput/Test-Project-1/Test-Project-1.rpp"
+$input_file=".\Reaper_Projects\CleanProjectInput\Test-Project-1\Test-Project-1.rpp"
 Write-Host $input_file
 
-# Get content of project file
-#$content = Get-Content -Path $input_file
+#Get just the path where the project file is located
+$input_file_path = Split-Path -Path $input_file
+
+# Get just the base filename of that path.  i.e. "project.rpp" gets just "project"
+$input_file_name = (Get-Item $input_file).Basename
+
+# Build a new project file name with the directory and new project file name
+$output_file_name = $input_file_name + "_render_stems" + ".rpp"
+
+# Build a complete payj to the new file
+$output_file_path = Join-Path -Path $input_file_path -ChildPath $output_file_name
+
+Write-Host $output_file_path
+
+# Copy the real project file to our new "_render_stems" project file
+Copy-Item $input_file $output_file_path
 
 #do replacements to override some render properties
-(Get-Content $input_file).replace('RENDER_STEMS 0', 'RENDER_STEMS 1') | Set-Content $input_file
+(Get-Content $output_file_path).replace('RENDER_STEMS 0', 'RENDER_STEMS 1') | Set-Content $output_file_path
 
 $Command = "C:\Program Files\REAPER (x64)\reaper.exe"
 $nosplash =  '-nosplash'
 $renderproject = '-renderproject'
 
-& $Command $nosplash $renderproject $input_file
+& $Command $nosplash $renderproject $output_file_path
